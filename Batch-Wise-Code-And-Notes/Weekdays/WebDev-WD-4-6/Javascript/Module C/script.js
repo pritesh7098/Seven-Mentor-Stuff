20aa74dda59dda5a7c6b7f9f768772f09be38ef5
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-09 17:39:29 +0530

    Started with module c in JS Batch

---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/Module C/script.js
@@ -0,0 +1,33 @@
+// Variables
+
+// var
+
+/* var age = 25; // age = 25
+console.log(age)
+
+var age = 26; 
+console.log(age); */
+
+// var is a global scope and can be redeclared
+// and can be reintialized
+
+/* ------------------------------------- */
+
+// let - local scope/block scope
+
+/* let age = 25;
+console.log(age);
+
+age = 26;
+console.log(age); */
+
+/* --------------------------------------------- */
+
+// const - local/block scope 
+
+const age = 24; 
+console.log(age);
+
+age = 6;
+
+console.log(age);
