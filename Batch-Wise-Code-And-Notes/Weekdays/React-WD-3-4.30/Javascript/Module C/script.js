3032e6d1ab602aceb70c13931db1bdcff8d4f977
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-09 17:38:59 +0530

    Started with module c in react batch

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/Module C/script.js
@@ -0,0 +1,28 @@
+// Variable
+
+// var
+
+/* var a = 2; 
+console.log(a);
+
+a = 3;
+console.log(a); */
+
+// let
+
+let name2 = "pritesh";
+console.log(name2);
+
+name2 = "rahul";
+console.log(name2);
+
+name2 = "nisha";
+console.log(name2);
+
+// const
+
+const age = 24;
+console.log(age);
+
+age = 25;
+console.log(age);
