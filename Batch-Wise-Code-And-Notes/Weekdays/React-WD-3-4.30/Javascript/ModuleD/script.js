8463e2699ca4c08a845ad5e3e0fdacdaa887064c
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-10 16:30:09 +0530

    Done with Module D and E

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/ModuleD/script.js
@@ -0,0 +1,19 @@
+// best practices
+
+let firstName = "om";
+let isLogin = true;
+
+// constants
+
+const PI = 3.14;
+const AGE = 24;
+
+// Functions
+
+function addOperation() {
+  // logic
+}
+
+// classes - pascal case
+
+class UserLogin {}
---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/ModuleE/script.js
@@ -0,0 +1,44 @@
+// primitive datatypes
+// number
+
+/* let age = 22;
+let price = 150.75;
+let invalid = NaN;
+let infinite = Infinity;
+ */
+// strings
+
+/* let name2 = "om shanti on";
+let fullName = `My full name is: ${name2} `;
+
+console.log(fullName); */
+
+// boolean
+
+/* let isLoggedIn = true;
+let isValid = false;
+ */
+// undefined
+
+/* let a = undefined;
+console.log(a); */
+
+// null 
+
+let data = null; 
+console.log(data);
+
+// bigint 
+
+let big = 987654321234567890n;
+console.log(big);
+
+// symbol 
+
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+console.log(id1 == id2); // false
+
+/* --------------------------------- */
+
+
