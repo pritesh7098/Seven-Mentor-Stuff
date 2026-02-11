8405f719ee84bd22964a35c692f1446ee511c1e9
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-11 20:02:12 +0530

    Completed Module F of JS

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/ModeuleF/script.js
@@ -0,0 +1,51 @@
+/* let age = 17;
+
+if (age >= 18) {
+  console.log("eligible to vote");
+} else {
+  console.log("not eligible");
+} */
+
+// else if - multiple conditions then use elseif
+
+/* let score = 82;
+
+if (score >= 90) {
+  console.log("A Grade");
+} else if (score >= 75) {
+  console.log("B Grade");
+} else if (score >= 60) {
+  console.log("C Grade");
+} else {
+  console.log("Fail");
+}
+ */
+// switch statement
+
+let day = 9;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tue");
+    break;
+
+  case 3:
+    console.log("Wed");
+    break;
+
+  case 4:
+    console.log("Thu");
+    break;
+
+  case 5:
+    console.log("Fri");
+    break;
+
+  default:
+    console.log("the day entered is an invalid day");
+  // fallback UI
+}
