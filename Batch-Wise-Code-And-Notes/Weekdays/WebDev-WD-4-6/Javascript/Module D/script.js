2ccb3896344c3844f887e1eb1949be6c36390504
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-11 20:01:05 +0530

    Completed module D and E of JS

---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/Module D/script.js
@@ -0,0 +1,22 @@
+// let a = 2;
+// var _name = "rahul";
+// const age = 24;
+
+// let return = "pritesh" // not valid
+// let _return = "pritesh" // valid syntax
+
+// best practices
+
+let userName = "rahul2001";
+let user_name = "rahul2001"; // both works
+
+isLoggedIn = true; // boolean
+
+const AGE = 25; // constants
+
+function printHello() {} // write the function in cc
+
+class WebDev {
+
+
+} // classes should be in pascal case 
---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/ModuleE/script.js
@@ -0,0 +1,41 @@
+// Datatypes
+
+// number
+
+let age = 24; // number
+
+let _name = "pritesh"; // string
+
+// typeof operator
+
+//console.log(typeof age);
+
+// boolean
+
+// true/false
+
+let isLoggedIn = 1;
+let isEligible = 2;
+
+//console.log(typeof isLoggedIn, typeof isEligible)
+
+// undefined - something is there but i dont' know what it is ?
+
+let a = undefined; // implicit or explicit
+//console.log(typeof a);
+
+// null
+
+let value = null;
+//console.log(typeof value); //  type of null = object
+
+// bigINT
+
+/* let numbers = 12345678999999999999999999999999999999999n;
+console.log(typeof numbers); */
+
+// symbol datatype ( ES6 newly introduced datatype)
+
+let id1 = Symbol("id");
+let id2 = Symbol("id");
+console.log(typeof id1); // false
