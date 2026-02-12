e9e9a2832ee67a5bf9bc83359f417bb297b9b3b1
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-12 16:26:39 +0530

    Completed module G Loops part

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/ModuleG/script.js
@@ -0,0 +1,40 @@
+// loops
+// for loop
+
+/* for(let i=6; i<=5; i++){
+
+    console.log("Number:", i);
+    
+
+
+
+} */
+
+// while loops
+
+/* let i = 6;
+
+while (6 <= 5) {
+  console.log("Number:", i);
+  i++; // inc and dec
+} */
+
+// do while
+
+/* let num = 5;
+
+do {
+    console.log("Executed once even if false");
+    num++;
+} while (num < 5);
+
+ */
+
+/* ----------------------------------------------------------------------- */
+
+// break
+
+for (let i = 1; i <= 5; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
