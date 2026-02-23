1aa0de108697e256673f1c653262efd6ebc97c9f
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-23 18:58:24 +0530

    Completed conditionals and loops part

---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/ModuleF/script.js
@@ -0,0 +1,62 @@
+// control strutures in Js
+
+// let age = 14;
+
+// if (age >= 18) {
+//   console.log("you are eligible to vote now ");
+// } else {
+//   console.log("not eligible ");
+// }
+
+// elseIf - when you have more than 2 condition to check
+
+// let marks = 77;
+
+// if (marks >= 90) {
+//   console.log("A");
+// } else if (marks >= 80) {
+//   console.log("B");
+// } else if (marks >= 70) {
+//   console.log("C");
+// } else if (marks >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("Fail");
+// }
+
+// ternary operator
+
+// ? :
+
+// if (age >= 18) {
+//   console.log("you are eligible to vote now ");
+// } else {
+//   console.log("not eligible ");
+// }
+
+// let age = 14;
+
+// let result = age>=18 ? "eligible" : "not eligible"
+// console.log(result);
+
+// switch statements
+
+
+let day = 20;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+
+  case 2:
+    console.log("Tuesday");
+    break;
+
+  case 3:
+    console.log("wednesday");
+    break;
+
+  default:
+    console.log("you have entered a wrong day");
+}
---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/ModuleG/script.js
@@ -0,0 +1,35 @@
+
+// for loop : 
+
+// for (let i = 0; i <= 10; i++) {
+//   // code
+
+//   console.log("Rahul");
+// } // no of iterations we know already
+
+/* ---------------------------------- */
+
+// While loop 
+
+// let i = 1;
+
+
+// while (i<=10) {
+    
+//     console.log("Number : ", i);
+//     i++;
+    
+
+// } 
+
+// do-while loops
+
+// let i = 0; // 0 
+
+// do {
+//     // code will execute atleast one time
+
+//     console.log("Number : ", i );
+//     i++ // 1,2,3 ----10 
+    
+// } while (i<=10); 0-10
