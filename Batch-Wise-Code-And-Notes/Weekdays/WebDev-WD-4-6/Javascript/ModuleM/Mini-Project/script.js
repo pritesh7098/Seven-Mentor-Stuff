27ce22959bc69c362a9a9bfca5875eeb4c2ac124
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-02-26 19:59:12 +0530

    Completed the dom part

---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-4-6/Javascript/ModuleM/Mini-Project/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("addBtn");
+
+btn.addEventListener("click", () => {
+  const input = document.getElementById("taskInput");
+  const list = document.getElementById("taskList");
+
+  const li = document.createElement("li");
+  li.innerText = input.value;
+
+  list.appendChild(li);
+  input.value = "";
+});
