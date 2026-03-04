4ab26b0f36e165520365c6a8d693eceb6edcc9ba
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-03-04 16:23:28 +0530

    completed callbacks and calback hell in JS

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/Javascript/ModuleJ-AsyncJS/script.js
@@ -0,0 +1,17 @@
+function order(isUserValid) {
+  function isUserValid() {
+    function buyProduct(order) {
+      function procceddToPay() {
+        // code
+        // code readbality 
+        // security 
+        // dependency 
+        // pyramid of doom 
+        // gives problem's in productions 
+
+      }
+    }
+  }
+}
+
+
