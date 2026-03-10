4c6625fe93d886f21e1cdebdff9470a59988a938
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-03-10 16:20:27 +0530

    First react app demonstration

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/ReactJS/First-React-APP/React-app/src/App.jsx
@@ -0,0 +1,43 @@
+/* import React from "react";
+
+// RFCE for normal function boilerplate
+
+function App() {
+  // actual code
+
+  let a = 2;
+  console.log(a);
+
+  let obj = {
+    name1: "pritesh",
+    age: 24,
+  };
+
+
+  // Written on UI 
+
+  return (
+    <>
+      <h1> a : {a} </h1>
+      <p>obj:{obj.name1}</p>
+      <p>{obj.age}</p>
+    </>
+  );
+}
+
+export default App;
+ */
+
+/* Arrow functions  */
+
+import React from "react";
+
+const App = () => {
+  // logic
+
+  // paint
+
+  return <div>App</div>;
+};
+
+export default App;
