d9b5a8a3b56b449e302c75c0599a70c70a14c40a
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-03-12 16:22:33 +0530

    Modified first react app

--- Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/ReactJS/First-React-APP/React-app/src/App.jsx
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/ReactJS/First-React-APP/React-app/src/App.jsx
@@ -1,43 +1,13 @@
-/* import React from "react";
-
-// RFCE for normal function boilerplate
+import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
-  // actual code
-
-  let a = 2;
-  console.log(a);
-
-  let obj = {
-    name1: "pritesh",
-    age: 24,
-  };
-
-
-  // Written on UI 
-
   return (
-    <>
-      <h1> a : {a} </h1>
-      <p>obj:{obj.name1}</p>
-      <p>{obj.age}</p>
-    </>
+    <div>
+      <h1>Hello From App.jsx</h1>
+      <Counter />
+    </div>
   );
 }
-
-export default App;
- */
-
-/* Arrow functions  */
-
-import React from "react";
-
-const App = () => {
-  // logic
-
-  // paint
-
-  return <div>App</div>;
-};
 
 export default App;
