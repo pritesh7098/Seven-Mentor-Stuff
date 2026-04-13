6052df32de440bf46106b23bb85c065f232b8393
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-04-13 17:44:57 +0530

    Completed loops

---
+++ Batch-Wise-Code-And-Notes/Weekdays/WebDev-WD-6.30-8/JavaScript/ModuleG/script.js
@@ -0,0 +1,40 @@
+/* console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+ */
+
+/* for (let i = 1; i <= 20; i++) {
+  console.log("Number", i);
+}
+ */
+
+// while loop
+
+/* let i = 1;
+
+while (i <= 20) {
+  console.log("Number:", i);
+  i++;
+} */
+
+// Do-while
+
+/* let num = 1;
+
+do {
+  console.log("This will run atleast once");
+  num++;
+} while (num < 5);
+ */ 
+
+// map 
+
+let numbers = [1,2,3,4]
+
+let squared = numbers.map(num => num*num*num  )
+console.log(squared);
+
+
+
