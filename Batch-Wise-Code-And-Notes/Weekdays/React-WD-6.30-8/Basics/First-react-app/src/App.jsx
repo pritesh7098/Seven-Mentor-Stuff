8940980c72910b4b73cdce22fef39e1e280b5bb5
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-04-28 17:45:21 +0530

    started with counter

--- Batch-Wise-Code-And-Notes/Weekdays/React-WD-6.30-8/Basics/First-react-app/src/App.jsx
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-6.30-8/Basics/First-react-app/src/App.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div>Rahul</div>;
+  // buisness logic
+
+  const [count] = useState(10); // react way
+  const count = 10; // js way
+
+  return (
+    <div>
+      {/* UI Part / UI Showcase  */}
+
+      <h1>Count : {count} </h1>
+      <button>Increment</button>
+      <br />
+      <button>Decrement</button>
+      <br />
+      <button>Reset</button>
+    </div>
+  );
 }
 
 export default App;
