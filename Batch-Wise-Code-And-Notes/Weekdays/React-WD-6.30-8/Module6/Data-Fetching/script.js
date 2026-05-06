d8fdca6398884818a31aefe74b88e2660b519921
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-05-06 19:38:42 +0530

    Started with data fetching

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-6.30-8/Module6/Data-Fetching/script.js
@@ -0,0 +1,25 @@
+/* Asynchronous operations */
+
+/* async function fetchUserData(user) {
+  const data = await fetch(`https://api.github.com/users/${user}`);
+
+  const finalResponse = await data.json();
+  console.log(finalResponse.url);
+}
+
+fetchUserData("pritesh7098"); */
+
+/* promisified*/
+
+function fetchUserData(user) {
+  const data = fetch(`https://api.github.com/users/${user}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.url);
+    })
+    .catch((error) => {
+      console.log("Something went wrong", error);
+    });
+}
+
+fetchUserData("pritesh7098");
