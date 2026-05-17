10ee70738e4ade4c8178e4bf1b34ad7fa4d31768
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-05-17 18:26:37 +0530

    added promises approach

--- Batch-Wise-Code-And-Notes/Weekend/ReactJs-Weekend/Module6/DataFetching/script.js
+++ Batch-Wise-Code-And-Notes/Weekend/ReactJs-Weekend/Module6/DataFetching/script.js
@@ -1,6 +1,6 @@
 // data fetching with javascript
 
-async function githubProfile(user) {
+/* async function githubProfile(user) {
   // actual logic
 
   const response = await fetch(`https://api.github.com/users/${user}`);
@@ -10,4 +10,21 @@ async function githubProfile(user) {
   console.log(finalResult.login);
 }
 
-githubProfile("pritesh7098");
+githubProfile("pritesh7098"); */
+
+// Promises approach - .then .catch  , promise chaining
+
+function getUserData(user) {
+  fetch(`https://api.github.com/users/${user}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.url);
+    })
+}
+
+getUserData("pritesh7098");
+
+
+
