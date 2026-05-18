2a38217c74b104632b890831acddd2d7b76959dc
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-05-18 19:57:44 +0530

    Updated

--- Batch-Wise-Code-And-Notes/Weekend/ReactJs-Weekend/Module6/DataFetching/dataFetching/src/App.jsx
+++ Batch-Wise-Code-And-Notes/Weekend/ReactJs-Weekend/Module6/DataFetching/dataFetching/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+/* import React, { useEffect, useState } from "react";
 
 function App() {
   // STEP1 - defining states
@@ -41,3 +41,47 @@ function App() {
 }
 
 export default App;
+ */
+
+
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+// Step 1
+
+const [post, setPost] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState (null);
+
+// step 2 
+
+useEffect (() => {
+   fetch ("https://jsonplaceholder.typicode.com/todos/1")
+   .then (response =>  response.json())
+   .then ((data) => {
+    setPost(data);
+    setLoading (false);
+   })
+   .catch ((error) => {
+    setError(error.message);
+    setLoading(false);
+   })
+}, [])
+
+// step 3 
+
+if (loading) return <p>Loading...</p>
+if (error) return <p>error : {error}</p>
+
+
+  return (
+    <div>
+      <h1>Data fetching in REACT</h1>
+      <h2>Id: {post.userId}</h2>
+      <h2>Title: {post.title}</h2>
+    </div>
+  )
+}
+
+export default App
