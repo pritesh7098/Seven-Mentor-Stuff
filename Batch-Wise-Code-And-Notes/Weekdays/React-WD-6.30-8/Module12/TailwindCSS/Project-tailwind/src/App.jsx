ae665675f9992772b7e7fb48bfa296a6bd04033e
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-05-25 20:00:23 +0530

    started with tailwind

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-6.30-8/Module12/TailwindCSS/Project-tailwind/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-500 p-40 m-20 text-2xl text-black border-2 border-blue-500 font-extrabold italic">
+        Learning Tailwind with vite
+      </h1>
+
+      <button>Click me </button>
+    </div>
+  );
+}
+
+export default App;
