e65ba8a1f0593333868378cd7d70ec434daa4a0f
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-06-07 19:42:58 +0530

    started with tailwind css

---
+++ Batch-Wise-Code-And-Notes/Weekend/ReactJs-Weekend/Module12/tailwind-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" md:bg-blue-500 lg:bg-red-500 p-2 m-2 font-bold italic ">
+        Hello to tailwind CSS
+      </h1>
+
+      <button className="border-2 border-green-400 ml-4 p-2 bg-amber-50 md:hover:bg-red-500">
+        Click Me{" "}
+      </button>
+    </div>
+  );
+}
+
+export default App;
