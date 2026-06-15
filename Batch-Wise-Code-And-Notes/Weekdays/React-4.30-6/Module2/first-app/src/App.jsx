d36b5d3e66f0bdfb1652e5c84f206cacd1b69834
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-06-15 17:47:05 +0530

    Started a fresh react project

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-4.30-6/Module2/first-app/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  // Buisness logic
+
+  // UI Showcase
+
+  return (
+    <>
+      <div>
+        <h1>Hello React app</h1>
+        <p>This is paragraph</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
