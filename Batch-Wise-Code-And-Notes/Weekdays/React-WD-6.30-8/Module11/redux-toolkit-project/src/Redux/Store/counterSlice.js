b6e0c15b45fd50932deeadd71daeed9671a5e1a5
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-06-17 19:58:46 +0530

    Redux toolkit explaination

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-6.30-8/Module11/redux-toolkit-project/src/Redux/Store/counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "Counter",
+  initialState: { value: 0 },
+
+  // Reducers means actaul actions to perform
+
+  increment: (state) => {
+    state.value = value += 1;
+  }, // value + 1
+  decrement: (state) => {
+    state.value = value -= 1;
+  },
+  reset: (state) => {
+    state.value = 0;
+  },
+});
+
+export const { increment, decrement, reset} = counterSlice.actions; // actions that we can use in store. 
+
+export default counterSlice.reducer; // reducer's are the actual function which we have to call whenever needed . 
+
+
