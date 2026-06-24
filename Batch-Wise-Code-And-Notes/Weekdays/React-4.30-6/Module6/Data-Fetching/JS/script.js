5992518ced3b1864e43faf75129e9408f865d68e
Pritesh7098 <priteshdhanad2001@gmail.com>
2026-06-24 17:59:22 +0530

    Done with JS data fetching

---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-4.30-6/Module6/Data-Fetching/JS/script.js
@@ -0,0 +1,30 @@
+// function to fetch data from server
+
+/* async function githubUserProfile(username) {
+  // logic
+
+  const reposnse = await fetch(`https://api.github.com/users/${username}`);
+
+  const finaldata = await reposnse.json();
+
+  console.log(finaldata);
+}
+
+githubUserProfile("octacat"); */
+
+/* ------------------------------------------------ */
+
+// Promises with .then and .catch
+
+function githubUserProfile() {
+  fetch(`https://api.github.com/users/pritesh7098`)
+    .then(
+      (response) => response.json(), // json form
+    )
+    .then((finaldata) => console.log(finaldata))
+    .catch((error) => {
+      console.log("Error while fetching data", error);
+    });
+}
+
+githubUserProfile();
