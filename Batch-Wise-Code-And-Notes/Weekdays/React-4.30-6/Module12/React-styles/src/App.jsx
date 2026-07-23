f55c1bd8bdb21d3f9e278366d2f0604c2a8642b2
pritesh7098 <priteshdhanad2001@gmail.com>
2026-07-23 17:07:36 +0530

    Completed the design in react

--- Batch-Wise-Code-And-Notes/Weekdays/React-4.30-6/Module12/React-styles/src/App.jsx
+++ Batch-Wise-Code-And-Notes/Weekdays/React-4.30-6/Module12/React-styles/src/App.jsx
@@ -8,6 +8,11 @@ function App() {
       <h1>Designing react app with css modules</h1>
       <Button />
       <Button2 />
+
+<h2 style={{backgroundColor:"red", textAlign:"center"}}>This is heading 2 </h2>
+
+<h2 className="p-2 m-2 bg-red text-center">This is h2 having tailwind utility classes</h2>
+
     </div>
   );
 }
---
+++ Batch-Wise-Code-And-Notes/Weekdays/React-4.30-6/Module12/React-styles/tailwind-demo/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bg-red-400 text-green-300 p-2 m-2 border-2 border-amber-700 font-bold">
+        Tailwind CSS Demo
+      </h1>
+
+      <br />
+
+      <button className="border-2 border-red-500 p-1 m-2 bg-sky-200 rounded hover:bg-yellow-300 ">
+        Click ME
+      </button>
+    </div>
+  );
+}
+
+export default App;
