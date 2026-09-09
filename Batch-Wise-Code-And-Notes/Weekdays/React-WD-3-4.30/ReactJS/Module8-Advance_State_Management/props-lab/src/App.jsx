73a8a22e52c2ceb147b64459329650cb16237be1
pritesh7098 <priteshdhanad2001@gmail.com>
2026-09-09 14:14:29 +0530

    Updated props

--- Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/ReactJS/Module8-Advance_State_Management/props-lab/src/App.jsx
+++ Batch-Wise-Code-And-Notes/Weekdays/React-WD-3-4.30/ReactJS/Module8-Advance_State_Management/props-lab/src/App.jsx
@@ -7,11 +7,9 @@ function App() {
   return (
     <div>
       <h1>Props and props drilling in reactJS</h1>
-      <Student name="pritesh" age="24" isEligible={true} />
-      <br />
-      <Student name="rahul" age="34" isEligible={false}/>
-      <br />
-      <br />
+      <Student name = "pritesh" age = "24" /> <br /><hr />
+      <Student name="rahul" age="36" /> <br /> <hr />
+      <Student name="shravani" age="22"/> 
     </div>
   );
 }
